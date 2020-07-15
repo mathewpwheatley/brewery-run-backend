9a6717ebde1984c8b170591ab631f82cbd58309b
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-15 16:31:06 -0700

    Create initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :breweries, except: [:new, :edit]
+  resources :brewery_favorites, only: [:create, :delete]
+  resources :brewery_likes, only: [:create, :delete]
+  resources :brewery_reviews, except: [:new, :edit]
+
+  resources :circuits, except: [:new, :edit]
+  resources :circuit_favorites, only: [:create, :delete]
+  resources :circuit_likes, only: [:create, :delete]
+  resources :circuit_reviews, except: [:new, :edit]
+
+  resources :follows, only: [:create, :delete]
+
+  resources :notifications, except: [:new, :edit]
+
+  resources :users, except: [:new, :edit]
+  post '/login', to: 'users#login'
+
 end
