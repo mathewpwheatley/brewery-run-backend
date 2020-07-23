918e13927bbf896bb9810819e2b069d6d04abc63
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-23 02:05:44 -0700

    Add data validation to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,11 @@ class Notification < ApplicationRecord
     belongs_to(:user)
 
     # ActiveRecord Validatons (See db schema for additional validators)
-    
+    validates(:title, presence: true)
+    validates(:title, length: { in: 10..50 })
+    validates(:content, presence: true)
+    validates(:content, length: { in: 10..100 })
+    validates(:user_id, presence: true)
+    validates(:user_id, numericality: { only_integer: true })
+
 end
