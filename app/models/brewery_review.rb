8de28dc478d8a4e3aa81fd54a521e7c73991514a
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-23 16:43:18 -0700

    removes redundant presence validators in reviews

--- app/models/brewery_review.rb
+++ app/models/brewery_review.rb
@@ -4,11 +4,8 @@ class BreweryReview < ApplicationRecord
     belongs_to(:user)
 
     # ActiveRecord Validatons (See db schema for additional validators)
-    validates(:title, presence: true)
     validates(:title, length: { in: 10..50 })
-    validates(:content, presence: true)
     validates(:content, length: { in: 50..500 })
-    validates(:rating, presence: true)
     validates(:rating, inclusion: { in: 0..5 })
     validates(:brewery_id, presence: true)
     validates(:brewery_id, numericality: { only_integer: true })
--- app/models/circuit_review.rb
+++ app/models/circuit_review.rb
@@ -4,11 +4,8 @@ class CircuitReview < ApplicationRecord
     belongs_to(:user)
 
     # ActiveRecord Validatons (See db schema for additional validators)
-    validates(:title, presence: true)
     validates(:title, length: { in: 10..50 })
-    validates(:content, presence: true)
     validates(:content, length: { in: 50..500 })
-    validates(:rating, presence: true)
     validates(:rating, inclusion: { in: 0..5 })
     validates(:circuit_id, presence: true)
     validates(:circuit_id, numericality: { only_integer: true })
