d13001a4d5e3a762806955020ece4a532c6800f0
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-31 22:29:31 -0700

    Updated cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Need to update this once it is published, this must be set for httponly cookies
     origins 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    # Need to update this once it is published, this must be set for httponly cookies
     origins 'https://brewery-run.herokuapp.com'
 
     resource '*',
